a85818ee2fa8ebbdf5a1aaa46f30eb6a9a00f9c3
Nicolis15 <Nicolistrabajo@gmail.com>
2025-04-06 18:43:48 -0500

    El amigo es una mierda

---
+++ maricas.js
@@ -0,0 +1,20 @@
+
+const pendejos = true
+
+function maricas() {
+    console.log("Que tan dificil puede ser crear una presentacion de canva en vez de complicacrse la vida")
+}
+function exelentes(){
+    console.log("La mejor opcion del mundo es usar canva")
+}
+
+const correr = (variable) => {
+    if(variable){
+        maricas()
+    }else{
+        exelentes()
+    }
+}
+
+correr(pendejos)
+
