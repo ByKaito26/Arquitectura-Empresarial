5205cc212f5c226296867a7e43ee3f741da358e8
TheLudway <theludway@gmail.com>
2025-04-06 23:45:25 -0500

    Diapositivas terminadas de mi parte

--- Presentacion/.auctex-auto/referencias.el
+++ Presentacion/.auctex-auto/referencias.el
@@ -7,6 +7,12 @@
     "MinTIC2021"
     "MinTIC2021marco"
     "MinTIC2021guiaimplementacion"
-    "bermejo2023claves"))
+    "bermejo2023claves"
+    "huawei2023gestion"
+    "Exact2022"
+    "ICDE2025"
+    "SRI2023"
+    "MinTIC_SoftwareLibre"
+    "MinTIC_MarcoInteroperabilidad"))
  '(or :bibtex :latex))
 
